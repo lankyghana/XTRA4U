441c4670ae4fdaf0c964bdca73ec9d5445cbe324
lankyghana <dktakyi001@st.ug.edu.gh>
2025-12-01 23:03:12 +0000

    feat: VendorFactory.php — added new code

---
+++ database/factories/VendorFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Vendor>
+ */
+class VendorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->userName,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone_number' => $this->faker->phoneNumber,
+            'password' => bcrypt('password'),
+            'is_approved' => true,
+        ];
+    }
+}
