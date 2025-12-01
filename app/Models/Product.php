ce830e69182dcfd0d885e89cdff4cc0ea5424d87
lankyghana <dktakyi001@st.ug.edu.gh>
2025-12-01 23:02:47 +0000

    feat: Product.php — added new code

---
+++ app/Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'vendor_id',
+        'name',
+        'description',
+        'price',
+        'is_active',
+    ];
+
+    public function vendor()
+    {
+        return $this->belongsTo(Vendor::class);
+    }
+}
