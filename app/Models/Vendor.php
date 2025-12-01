d22508a590987f52ffaf4b5c2149d82241681808
lankyghana <dktakyi001@st.ug.edu.gh>
2025-12-01 23:02:49 +0000

    feat: Vendor.php — added new code

---
+++ app/Models/Vendor.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Vendor extends Authenticatable
+{
+    use HasFactory;
+
+    // Vendor has many orders
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    // Vendor has many products
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone_number',
+        'password',
+        'is_approved',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    protected $casts = [
+        'is_approved' => 'boolean',
+    ];
+}
