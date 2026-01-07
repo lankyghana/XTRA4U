5421dc34f4cc36d3a17988bd7481ed1221358be5
lankyghana <dktakyi001@st.ug.edu.gh>
2026-01-07 22:51:10 +0000

    config: services.php — updated implementation

--- config/services.php
+++ config/services.php
@@ -66,7 +66,7 @@ return [
     'bulkclix' => [
         'api_key' => env('BULKCLIX_API_KEY'),
         'sender_id' => env('BULKCLIX_SENDER_ID', 'XTRA4U'),
-        'base_url' => env('BULKCLIX_BASE_URL', 'https://bulkclix.com/api/v1'),
+        'base_url' => env('BULKCLIX_BASE_URL', 'https://api.bulkclix.com/api/v1'),
     ],
 
     'paystack' => [
